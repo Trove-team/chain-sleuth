1ab567bcfeb4eeef0cdc8aa696d60a1f4974ee0f
Brandon Cantu <team@trovelabs.xyz>
2024-10-20 18:22:24 -0700

    ugh

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,10 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import app from '@/api';  // Import your Elysia app
+import { GET, POST } from '@/api';
 
-export async function GET(request: NextRequest) {
-  return app.handle(request);
-}
-
-export async function POST(request: NextRequest) {
-  return app.handle(request);
-}
+export { GET, POST };
