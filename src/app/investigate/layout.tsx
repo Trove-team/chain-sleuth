97da759827b19669b0e4a3bb2daa29335014b3bc
Trove-team <team@trovelabs.xyz>
2024-11-01 10:25:16 +0900

    investigate page

--- src/app/investigate/layout.tsx
+++ src/app/investigate/layout.tsx
@@ -1,11 +1,11 @@
-// src/app/queries/layout.tsx
+// src/app/investigate/layout.tsx
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-export default function QueriesLayout({
+export default function InvestigateLayout({
   children,
 }: {
   children: React.ReactNode;
